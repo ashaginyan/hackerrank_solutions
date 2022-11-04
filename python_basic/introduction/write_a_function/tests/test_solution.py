58c8e08f4fe895ec088af67000cc29cf816c5948
Anaida <anaidashagininan@gmail.com>
2022-11-05 02:58:20 +0300

    add write_a_function

---
+++ python_basic/introduction/write_a_function/tests/test_solution.py
@@ -0,0 +1,5 @@
+from solution import is_leap
+
+def test_is_leap():
+    assert is_leap(1990) == False
+    assert is_leap(2000) == True
---
+++ python_basic/introduction/write_a_function/tests/test_write_a_function.py
@@ -0,0 +1,5 @@
+from write_a_function import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
